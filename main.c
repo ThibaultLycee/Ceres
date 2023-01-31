aa17cdf0927a9450b6f3436866e0bb2e66fda48f
ThibaultLycee <thib.durand@free.fr>
2023-01-31 18:24:24 +0100

    Added gitignore

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 #include "ceres.c"
 
-#define WIDTH 500
-#define HEIGHT 500
+#define WIDTH 50
+#define HEIGHT 50
 
 #define IMG_SIZE (WIDTH * HEIGHT)
 
@@ -13,10 +13,10 @@ static Color32 pixels[IMG_SIZE];
 int main(void) {
 	Ceres_FillPixels(pixels, IMG_SIZE, C_WHITE);
 
-	pixels[200 * WIDTH + 50] = C_RED;
-	pixels[100 * WIDTH + 150] = C_RED;
-	pixels[300 * WIDTH + 450] = C_RED;
-	Ceres_Bezier2(pixels, WIDTH, HEIGHT, 50, 200, 150, 100, 450, 300, C_BLACK);
+	pixels[20 * WIDTH + 5] = C_RED;
+	pixels[10 * WIDTH + 15] = C_RED;
+	pixels[30 * WIDTH + 45] = C_RED;
+	Ceres_Bezier2(pixels, WIDTH, HEIGHT, 5, 20, 15, 10, 45, 30, C_BLACK);
 
 	Ceres_SavePpm(pixels, WIDTH, HEIGHT);
 	return 0;
